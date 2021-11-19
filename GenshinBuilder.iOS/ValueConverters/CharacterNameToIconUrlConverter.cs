b9c8a4cd0d758e4dbccf1f109e7759095af378e5
Gabriel Ferreiro <gbril9119@gmail.com>
2021-11-19 16:45:24 +0100

    Sabe dios por que faltaban archivos por agregrar

---
+++ GenshinBuilder.iOS/ValueConverters/CharacterNameToIconUrlConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace GenshinBuilder.iOS.ValueConverters
+{
+    public class CharacterNameToIconUrlConverter : MvxValueConverter<string, string>
+    {
+        public CharacterNameToIconUrlConverter()
+        {
+        }
+
+        protected override string Convert(string value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"https://api.genshin.dev/characters/{value}/icon";
+        }
+    }
+}
